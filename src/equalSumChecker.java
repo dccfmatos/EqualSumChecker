bd8240eeb04658f6e81409cb46b3af1e799715df
dccfmatos <d.catarina.c.f@gmail.com>
2019-05-04 19:05:51 +0100

    Initial commit

---
+++ src/equalSumChecker.java
@@ -0,0 +1,10 @@
+public class equalSumChecker {
+
+    public static boolean hasEqualSum(int numb1, int numb2, int numb3){
+
+        if (numb1+numb2 == numb3){
+            return true;
+        }
+        return false;
+    }
+}
